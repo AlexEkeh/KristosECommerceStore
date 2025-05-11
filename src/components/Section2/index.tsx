50cd674b27157f7684ce76304d4c1c2243b71ee8
Olumide Ojo <olumide.m.ojo@gmail.com>
2025-05-11 19:31:38 +0100

    perf: modify product category component

--- src/components/Section2/index.tsx
+++ src/components/Section2/index.tsx
@@ -35,7 +35,7 @@ const Section2 = () => {
         <TopContent>
           {ProductCategories.map((product) => (
             <TopContentItem key={product.id}>
-              <Product>
+              <Product p={2} width={"197px"} height={"211px"} gap={1}>
                 <Box
                   sx={{
                     display: "flex",
@@ -51,10 +51,11 @@ const Section2 = () => {
                 <Box
                   sx={{
                     display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "flex-end",
-                    height: "85%",
-                    // border: "1px solid red"
+                    alignItems: "center",
+                    justifyContent: "center",
+                    height: "211px",
+                    width: "197px",
+                    // border: "1px solid red",
                   }}
                 >
                   <Image
