8e9c05b083e25be201563cee4a38a1898bc07c0b
Olumide Ojo <olumide.m.ojo@gmail.com>
2025-05-13 12:56:24 +0100

    feat: add Inter font to the page layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,7 +2,13 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import Main from "@/components/Main";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import '../globals.css';
+import "../globals.css";
+import { Inter } from "next/font/google"; //Olumide: I import the Inter Font
+
+//Olumide: I declare a geistInter Variable
+const geistInter = Inter({
+  subsets: ["latin"],
+});
 
 export default function HomeLayout({
   children,
@@ -10,7 +16,8 @@ export default function HomeLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={geistInter.className}>
+      {/*Olumide: i adde the inter font*/}
       <body>
         <AppRouterCacheProvider>
           <Main>
