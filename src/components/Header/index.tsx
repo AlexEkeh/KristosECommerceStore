1c49ccbd7c6e36069d8a6cb34173d58728d06284
Olumide Ojo <olumide.m.ojo@gmail.com>
2025-05-13 17:18:25 +0100

    style: set max width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,24 @@
 "use client";
 import BrandLogo from "@/Icons/BrandLogo";
 import Toolbar from "@mui/material/Toolbar";
-import { HeadContainer, Left, Right } from "./style";
+import { HeadContainer, HeadContainerWrapper, Left, Right } from "./style";
 import Search from "../Search";
 
 const Header = () => {
   return (
     <HeadContainer position="static" elevation={0}>
-      <Toolbar
-        disableGutters
-        sx={{ display: "flex", alignItems: "center", height: "100%" }}
-      >
-        <Left>
-          <BrandLogo />
-          <Search />
-        </Left>
-        <Right></Right>
-      </Toolbar>
+      <HeadContainerWrapper>
+        <Toolbar
+          disableGutters
+          sx={{ display: "flex", alignItems: "center", height: "100%" }}
+        >
+          <Left>
+            <BrandLogo />
+            <Search />
+          </Left>
+          <Right></Right>
+        </Toolbar>
+      </HeadContainerWrapper>
     </HeadContainer>
   );
 };
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,33 +1,37 @@
 import { styled } from "@mui/material/styles";
-import AppBar from '@mui/material/AppBar';
+import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 
 const HeadContainer = styled(AppBar)`
-    width: 100%;
-    height: 100%; // height: 80px
-    background-color: #ffffff;
-    /* border: 1px solid green; */
+  width: 100%;
+  height: 100%; // height: 80px
+  background-color: #ffffff;
+  /* border: 1px solid green; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeadContainerWrapper = styled(Box)`
+  width: 100%;
+  max-width: 1600px;
+  height: 100%;
+  //   border: 1px solid green;
 `;
 
 const Left = styled(Box)`
-    width: 40%;
-    height: 100%;
-    /* border: 1px solid green; */
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
+  width: 40%;
+  height: 100%;
+  /* border: 1px solid green; */
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
 const Right = styled(Box)`
-    height: 100%;
-    flex-grow: 1;
-    border: 1px solid red;
+  height: 100%;
+  flex-grow: 1;
+  border: 1px solid red;
 `;
 
-
-
-export {
-    HeadContainer,
-    Left,
-    Right
-}
+export { HeadContainer, HeadContainerWrapper, Left, Right };
