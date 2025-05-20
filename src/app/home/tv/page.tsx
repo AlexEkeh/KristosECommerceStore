63d8df509b2567f75b512177d9b0e7c2d0d58452
Olumide Ojo <olumide.m.ojo@gmail.com>
2025-05-20 06:57:27 +0100

    feat: add corevalue and brief section

--- src/app/home/tv/page.tsx
+++ src/app/home/tv/page.tsx
@@ -1,10 +1,32 @@
 "use client";
-import React from 'react'
+import { ItemContainer } from "@/components/Global/ItemContainer";
+import { SectionContainer } from "@/components/Global/SectionContainer";
+import BriefSection from "@/section/BriefSection";
+import CoreValueSection from "@/section/CoreValueSection";
+import Box from "@mui/material/Box";
+import React from "react";
 
 const TV = () => {
   return (
-    <div>TV</div>
-  )
-}
+    <Box
+      sx={{
+        width: "100%",
+        height: "fit-content",
+        overflow: "hidden",
+        maxWidth: "1600px",
+      }}
+    >
+      <SectionContainer>
+        <ItemContainer>TV</ItemContainer>
+        <ItemContainer>
+          <BriefSection />
+        </ItemContainer>
+        <ItemContainer>
+          <CoreValueSection />
+        </ItemContainer>
+      </SectionContainer>
+    </Box>
+  );
+};
 
-export default TV
+export default TV;
