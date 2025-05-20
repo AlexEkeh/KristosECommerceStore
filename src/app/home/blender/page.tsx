832135a4732c55b383ff36c383c72039cb0e5fd9
Olumide Ojo <olumide.m.ojo@gmail.com>
2025-05-20 06:58:24 +0100

    feat: add corevalue and brief section

--- src/app/home/blender/page.tsx
+++ src/app/home/blender/page.tsx
@@ -1,10 +1,32 @@
 "use client";
-import React from 'react'
+import { ItemContainer } from "@/components/Global/ItemContainer";
+import { SectionContainer } from "@/components/Global/SectionContainer";
+import BriefSection from "@/section/BriefSection";
+import CoreValueSection from "@/section/CoreValueSection";
+import Box from "@mui/material/Box";
+import React from "react";
 
 const Blender = () => {
   return (
-    <div>Blenders</div>
-  )
-}
+    <Box
+      sx={{
+        width: "100%",
+        height: "fit-content",
+        overflow: "hidden",
+        maxWidth: "1600px",
+      }}
+    >
+      <SectionContainer>
+        <ItemContainer>Blenders</ItemContainer>
+        <ItemContainer>
+          <BriefSection />
+        </ItemContainer>
+        <ItemContainer>
+          <CoreValueSection />
+        </ItemContainer>
+      </SectionContainer>
+    </Box>
+  );
+};
 
-export default Blender
+export default Blender;
