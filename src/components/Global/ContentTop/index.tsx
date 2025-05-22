e8f0a0931e92bfc8289a6961a8dc584e1688b476
Olumide Ojo <olumide.m.ojo@gmail.com>
2025-05-22 14:05:54 +0100

    perf: add function to view menu

--- src/components/Global/ContentTop/index.tsx
+++ src/components/Global/ContentTop/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import {
   ContentTopCategory,
   ContentTopContainer,
@@ -9,6 +9,7 @@ import {
   ContentTopTextBox,
   ContentTopViewIcon,
   ContentTopViewIconWrapper,
+  ContentTopWrapper,
 } from "./style";
 import WindowIcon from "@mui/icons-material/Window";
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
@@ -17,39 +18,53 @@ import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 
 interface ContentTopProps {
   category: string;
-  itemNo: number | null | undefined;
+  itemNo: number | null;
+  viewIcon: ReactNode;
+  menuIcon: ReactNode;
+  onViewIconClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  onMenuIconClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
-export const ContentTop = ({ category, itemNo }: ContentTopProps) => {
+export const ContentTop = ({
+  category,
+  itemNo,
+  viewIcon,
+  menuIcon,
+  onViewIconClick,
+  onMenuIconClick,
+}: ContentTopProps) => {
   const [isGridView, setIsGridView] = useState(true);
   const [isOpen, setIsOpen] = useState(true);
+
   return (
     <ContentTopContainer>
-      <ContentTopTextBox>
-        <ContentTopCategory>
-          {category ? category : "Category"}
-        </ContentTopCategory>
-        <ContentTopItemSummary>
-          {itemNo != null && itemNo !== 0 ? itemNo : null}{" "}
-          {itemNo != null && itemNo !== 0
-            ? `${itemNo > 1 ? "Items found" : "item found"}`
-            : `No items found`}
-        </ContentTopItemSummary>
-      </ContentTopTextBox>
+      <ContentTopWrapper>
+        <ContentTopTextBox>
+          <ContentTopCategory>
+            {category ? category : "Category"}
+          </ContentTopCategory>
+          <ContentTopItemSummary>
+            {itemNo != null && itemNo !== 0 ? itemNo : null}{" "}
+            {itemNo != null && itemNo !== 0
+              ? `${itemNo > 1 ? "Items found" : "item found"}`
+              : `No items found`}
+          </ContentTopItemSummary>
+        </ContentTopTextBox>
 
-      <ContentTopMenuViewBox>
-        <ContentTopViewIconWrapper>
-          <ContentTopViewIcon onClick={() => setIsGridView((prev) => !prev)}>
-            {isGridView ? <WindowIcon /> : <FormatListBulletedIcon />}
-          </ContentTopViewIcon>
-        </ContentTopViewIconWrapper>
+        <ContentTopMenuViewBox>
+          <ContentTopViewIconWrapper>
+            <ContentTopViewIcon onClick={onViewIconClick}>
+              {viewIcon}
+            </ContentTopViewIcon>
+          </ContentTopViewIconWrapper>
 
-        <ContentTopMenuIconWrapper>
-          <ContentTopMenuIcon onClick={() => setIsOpen((prev) => !prev)}>
-            {isOpen ? <MenuIcon /> : <MenuOpenIcon />}
-          </ContentTopMenuIcon>
-        </ContentTopMenuIconWrapper>
-      </ContentTopMenuViewBox>
+          <ContentTopMenuIconWrapper>
+            <ContentTopMenuIcon onClick={onMenuIconClick}>
+              {menuIcon}
+            </ContentTopMenuIcon>
+          </ContentTopMenuIconWrapper>
+        </ContentTopMenuViewBox>
+      </ContentTopWrapper>
     </ContentTopContainer>
   );
 };
