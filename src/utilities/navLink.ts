5d7488bf32768da73600683248f4fad83dc2fb8e
Olumide Ojo <olumide.m.ojo@gmail.com>
2025-05-27 16:16:46 +0100

    feat: create sub link data

---
+++ src/utilities/navLink.ts
@@ -0,0 +1,12 @@
+export const navCategoryLinks = [
+  { name: "Freezers", href: "/home/freezers" },
+  { name: "Cookers", href: "/home/cookers" },
+  { name: "Washing Machine", href: "/home/washing_machine" },
+  { name: "TV", href: "/home/tv" },
+  { name: "Air Conditioners", href: "/home/air_condition" },
+  { name: "Generator", href: "/home/generator" },
+  { name: "Kettle", href: "/home/kettle" },
+  { name: "Iron", href: "/home/iron" },
+  { name: "Blenders", href: "/home/blenders" },
+  { name: "Extensions", href: "/home/extensions" },
+];
